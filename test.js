67fcb5a2f91170ecd2984de74ae68960f30e88f4
Sneha Kumari <snehabaranwal5613@gmail.com>
2025-01-15 19:55:12 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+const linkedIn = require("./index");
+
+const queryOptions = {
+  keyword: "",
+  location: "India",
+  dateSincePosted: "past Week",
+  jobType: "full time",
+  remoteFilter: "remote",
+  salary: "100000",
+  experienceLevel: "entry level",
+  limit: "1",
+  sortBy: "recent",
+  page: "1",
+};
+
+linkedIn.query(queryOptions).then((response) => {
+  console.log(response); // An array of Job objects
+});
